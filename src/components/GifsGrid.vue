a52a9f7f377250181d7ffb6495cd0cf39a063946
Inti Dev <yeseniase@outlook.it>
2020-08-30 19:42:49 -0500

    [ADD] media queries

--- src/components/GifsGrid.vue
+++ src/components/GifsGrid.vue
@@ -31,12 +31,6 @@
         src="https://media2.giphy.com/media/S9RUTgRxiFVEpOQ4jW/giphy.gif?cid=ddf33540fp5qn5nx2ms5msu5rvin8iv9y2678c8armq5w8mh&rid=giphy.gif"
       />
       </div>
-
-      <div class="Grid-item">
-        <img
-        src="https://media2.giphy.com/media/S9RUTgRxiFVEpOQ4jW/giphy.gif?cid=ddf33540fp5qn5nx2ms5msu5rvin8iv9y2678c8armq5w8mh&rid=giphy.gif"
-      />
-      </div>
     </div>
   </section>
 </template>
@@ -58,7 +52,7 @@ p {
 
 .Grid-items-container {
   display: grid;
-  grid-template-columns: repeat(5, 20%);
+  grid-template-columns: repeat(5, 1fr);
   margin-top: 20px;
   padding: 0 10px;
 }
@@ -71,4 +65,22 @@ img {
   max-width: 100%;
 }
 
+
+@media (max-width: 992px) {
+  .Grid-items-container {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
+@media (max-width: 768px) {
+  .Grid-items-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (max-width: 480px) {
+  .Grid-items-container {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
 </style>
